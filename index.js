97eba966ef7f2c6975d0ab7bf9c5baa6ae94a445
Anderson Viana <andersonkallian@yahoo.com.br>
2022-10-01 22:36:00 -0300

    adding js functionalities

--- index.js
+++ index.js
@@ -1,33 +1,94 @@
-const loadMap = function (id) {
-
-  let center = [60.1708, 24.9375];
-  let map = L.map(id);
-  let tile_url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-  let layer = L.tileLayer(tile_url, {
-      attribution: 'OSM'
-  });
-
-  map.addLayer(layer);
-  map.setView(center, 19);
-
-  map.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
-    //   .on('locationfound', function(e){
-    //     let marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
-    //     let circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
-    //           weight: 1,
-    //           color: 'blue',
-    //           fillColor: '#cacaca',
-    //           fillOpacity: 0.2
-    //       });
-    //       map.addLayer(marker);
-    //       map.addLayer(circle);
-    //   })
-    //  .on('locationerror', function(e){
-    //       console.log(e);
-    //       alert("Location access denied.");
-    //   });
-};
-
-loadMap('map');
+const APIKEY = "at_cXd6xETxwI0ICbnqdn3Sv07CfbL08";
+const ip = document.querySelector(".ip-number");
+const btn = document.querySelector(".submit");
 
 
+let showIP = document.querySelector("#id")
+let showLocation = document.querySelector("#location");
+let showTimezone = document.querySelector("#timezone");
+let showISP = document.querySelector("#isp");
+let container = L.DomUtil.get('map');
+
+
+
+
+
+btn.addEventListener("click", getIp);
+
+
+
+
+/* Get user IP address on page load */
+window.onload = () => fetchIp();
+
+
+function fetchIp() {
+    fetch("https://geo.ipify.org/api/v2/country,city?apiKey=".concat(APIKEY, "&ipAddress=").concat(ip.value))
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+
+        showIP.innerText = data.ip;
+        showISP.innerText = data.isp;
+        showTimezone.innerText = "UTC ".concat(data.location.timezone);
+        showLocation.innerText = `${data.location.region}, ${data.location.city}, ${data.location.country}, ${data.location.postalCode}`;
+
+        /* Leaflet Map JS */
+
+        let map = L.map('map').setView([data.location.lat, data.location.lng], 13);
+        let tileURL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+        let osm = L.tileLayer(tileURL, {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        });
+        const icon = L.icon({
+          iconUrl: "./images/icon-location.svg",
+          iconSize: [46, 56], // size of the icon
+        });
+        osm.addTo(map);
+        position = [data.location.lat, data.location.lng];
+        L.marker(position, {icon: icon}).addTo(map);
+        
+    });
+}
+
+
+ip.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    getIp()
+  }
+});
+
+function getIp(){
+  fetch("https://geo.ipify.org/api/v2/country,city?apiKey=".concat(APIKEY, "&ipAddress=").concat(ip.value))
+        .then(res => res.json())
+        .then(data => {
+        
+        
+        container._leaflet_id = null;
+
+        showIP.innerText = data.ip;
+        showISP.innerText = data.isp;
+        showTimezone.innerText = "UTC ".concat(data.location.timezone);
+        showLocation.innerText = `${data.location.region}, ${data.location.city}, ${data.location.country}, ${data.location.postalCode}`;
+
+
+        
+        let map = L.map('map').setView([data.location.lat, data.location.lng], 13);
+        let tileURL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+        let osm = L.tileLayer(tileURL, {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        });
+        const icon = L.icon({
+          iconUrl: "./images/icon-location.svg",
+          iconSize: [46, 56], // size of the icon
+        });
+        osm.addTo(map);
+        position = [data.location.lat, data.location.lng];
+        L.marker(position, {icon: icon}).addTo(map);
+        map.flyTo([data.location.lat, data.location.lng], 13)
+  })
+
+}
